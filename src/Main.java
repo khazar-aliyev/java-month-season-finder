83557cefedc9aa60dfccf4f5390bb95820d8864f
Khazar Aliyev <aliyev.xezer@yahoo.com>
2026-08-17 00:38:49 +0400

    Refactor Main class and implement seasonal output

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("In which month you are? ");
+        String month = scanner.nextLine().toLowerCase();
+        switch (month) {
+            case "january", "february", "december" -> System.out.println("It is winter");
+            case "march", "april", "may" -> System.out.println("It is spring");
+            case "june", "july", "august" -> System.out.println("It is summer");
+            case "september", "october", "november" -> System.out.println("It is autumn");
+            default -> System.out.println(month + " is not a month");
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+        }
+
+    }
 }
